6cb8976fc092cb09bf5ae3844b216f2fb19efadd
Piotr Soluch <psoluch@wiredot.com>
2016-09-04 23:58:09 +0200

    new CPT class

---
+++ classes/CPT.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Preamp;
+
+class CPT {
+
+	private $post_type;
+	private $args;
+	
+	function __construct($post_type) {
+		$this->post_type = $post_type;
+
+		add_action( 'init', array( $this, 'register_post_type' ) );
+	}
+
+	public function register_post_type() {
+		echo $this->post_type;
+		exit;
+	}
+}
