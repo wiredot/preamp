71f3237abd6b628ebccec49e0c2aaa53520ed390
Piotr Soluch <psoluch@wiredot.com>
2016-11-26 22:36:49 +0100

    saving metaboxes

--- classes/Meta_Boxes/Meta_Box_Factory.php
+++ classes/Meta_Boxes/Meta_Box_Factory.php
@@ -7,19 +7,32 @@ class Meta_Box_Factory {
 	private $meta_boxes;
 	
 	function __construct($meta_boxes) {
-		if (is_array($meta_boxes)) {
-			foreach ($meta_boxes as $meta_box_id => $meta_box) {
-				
-				switch ($meta_box['type']) {
-					case 'post':
-						new Post_Meta_Box($meta_box_id, $meta_box);
-						break;
-					
-					default:
-						# code...
-						break;
+		if ( ! is_array($meta_boxes) ) {
+			return;
+		}
+
+		foreach ($meta_boxes as $meta_box_id => $meta_box) {
+			if (is_array($meta_box['post_type'])) {
+				foreach ($meta_box['post_type'] as $post_type) {
+					$new_meta_box = $meta_box;
+					$new_meta_box['post_type'] = $post_type;
+					$this->init_meta_box($meta_box['type'], $meta_box_id, $new_meta_box);
 				}
+			} else {
+				$this->init_meta_box($meta_box['type'], $meta_box_id, $meta_box);
 			}
+		}
+	}
+
+	public function init_meta_box($type, $meta_box_id, $meta_box) {
+		switch ($type) {
+			case 'post':
+				new Post_Meta_Box($meta_box_id, $meta_box);
+				break;
+			
+			default:
+				# code...
+				break;
 		}
 	}
 }
--- classes/Meta_Boxes/Post_Meta_Box.php
+++ classes/Meta_Boxes/Post_Meta_Box.php
@@ -13,7 +13,11 @@ class Post_Meta_Box extends Meta_Box {
 		$this->meta_box_id = $meta_box_id;
 		$this->meta_box = $meta_box;
 
+		// add meta boxes
 		add_action('admin_init', array($this, 'add_meta_box'));
+
+		// save meta boxes
+		add_action('save_post_'.$meta_box['post_type'], array($this, 'save_meta_box'), 10, 3);
 	}
 
 	public function add_meta_box() {
@@ -28,7 +32,7 @@ class Post_Meta_Box extends Meta_Box {
 		);
 	}
 
-	public function add_meta_box_content() {
+	public function add_meta_box_content($post, $meta_box) {
 		if (is_array($this->meta_box['fields'])) {
 			foreach ($this->meta_box['fields'] as $key => $meta_box_field) {
 				if ( ! isset($meta_box_field['attributes']) ) {
@@ -39,9 +43,48 @@ class Post_Meta_Box extends Meta_Box {
 					$meta_box_field['options'] = array();
 				}
 
-				$field = new Field_Factory($meta_box_field['type'], $key, $key, 'asd', $meta_box_field);
+				$value = get_post_meta( $post->ID, $key, true );
+
+				$field = new Field_Factory($meta_box_field['type'], $key, $key, $value, $meta_box_field);
 				$field->showField();
 			}
+		}
+	}
+
+	public function save_meta_box($post_id, $post, $update) {
+		// return if autosave
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+			return;
+		}
+
+		// if ( ! isset( $_POST['preamp_mb_'.$this->meta_box_id.'_nonce'] ) || ! wp_verify_nonce( $_POST['preamp_mb_'.$this->meta_box_id.'_nonce'], 'wp_verify_nonce' ) ){
+		// 	return;
+		// }
+
+		foreach ($this->meta_box['fields'] as $key => $field) {
+			switch ($field['type']) {
+				default:
+					$this->save_meta_box_field($post_id, $key, $field['type']);
+					break;
+			}
+		}
+	}
+
+	public function save_meta_box_field($post_id, $key, $field_type) {
+		if ( isset( $_POST[$key] ) ){
+			$value = $_POST[$key];
+
+			switch ($field_type) {
+				default:
+					$sanitized_value = sanitize_text_field($value);
+					break;
+			}
+
+			// save data
+			update_post_meta( $post_id, $key, $sanitized_value );
+		} else {
+			// delete data
+			delete_post_meta( $post_id, $key );
 		}
 	}
 }
