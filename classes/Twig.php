e3d5b1e2aeb9d41d6b1ec7e8b5028689825b22a1
Piotr Soluch <psoluch@wiredot.com>
2016-11-29 23:40:19 +0100

    new image class

--- classes/Twig.php
+++ classes/Twig.php
@@ -5,6 +5,7 @@ namespace Wiredot\Preamp;
 use Twig_Loader_Filesystem;
 use Twig_Environment;
 use Twig_Extension_Debug;
+use Twig_SimpleFunction;
 
 class Twig {
 
@@ -19,5 +20,10 @@ class Twig {
     		'debug' => true
 		));
 		$this->twig->addExtension(new Twig_Extension_Debug());
+		$function = new Twig_SimpleFunction('image', function ($image_id, $params = array(), $attributes = array()) {
+    		$Image = new Image($image_id, $params, $attributes);
+    		return $Image->get_image();
+		});
+		$this->twig->addFunction($function);
 	}
 }
