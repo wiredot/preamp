f676b44308a504edb92345d45b2d731cddfbdee7
Piotr Soluch <psoluch@wiredot.com>
2016-12-04 01:15:01 +0100

    added support for cpt messages

--- classes/Custom_Post_Types/Custom_Post_Type.php
+++ classes/Custom_Post_Types/Custom_Post_Type.php
@@ -11,10 +11,28 @@ class Custom_Post_Type {
 		$this->post_type = $post_type;
 		$this->args = $args;
 
+		if (isset($args['messages'])) {
+			add_filter( 'post_updated_messages', array( $this, 'updated_messages' ) );
+		}
 		add_action( 'init', array( $this, 'register_post_type' ) );
 	}
 
 	public function register_post_type() {
 		register_post_type( $this->post_type, $this->args );
+	}
+
+	public function updated_messages($messages) {
+		$post = get_post();
+
+		$new_messages = $this->args['messages'];
+		$date = date_i18n( __( 'M j, Y @ G:i', 'wp-photo-gallery' ), strtotime( $post->post_date ));
+		$new_messages[9] = str_replace("%date%", $date, $new_messages[9]);
+		if (isset($_GET['revision'])) {
+			$new_messages[5] = str_replace("%revision%", $_GET['revision'], $new_messages[5]);
+		}
+
+		$messages['wp-photo-gallery'] = $new_messages;
+
+		return $messages;
 	}
 }
