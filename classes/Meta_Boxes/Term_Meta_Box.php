905a366841ca9c35183961373987e0812b4f632f
Piotr Soluch <psoluch@wiredot.com>
2017-11-29 23:55:24 +0100

    save term meta

--- classes/Meta_Boxes/Term_Meta_Box.php
+++ classes/Meta_Boxes/Term_Meta_Box.php
@@ -15,10 +15,13 @@ class Term_Meta_Box extends Meta_Box {
 		if ( is_array( $meta_box['taxonomy'] ) ) {
 			foreach ( $meta_box['taxonomy'] as $taxonomy ) {
 				add_action( $taxonomy . '_add_form_fields', array( $this, 'add_form_fields' ), 10, 3 );
+				add_action( $taxonomy . '_edit_form_fields', array( $this, 'edit_form_fields' ), 10, 2 );
 			}
 		} else {
 			add_action( $meta_box['taxonomy'] . '_add_form_fields', array( $this, 'add_form_fields' ), 10, 3 );
 			add_action( $meta_box['taxonomy'] . '_edit_form_fields', array( $this, 'edit_form_fields' ), 10, 2 );
+
+			add_action( 'edited_' . $meta_box['taxonomy'], array( $this, 'update_term_meta' ), 10, 2 );
 		}
 	}
 
@@ -59,6 +62,19 @@ class Term_Meta_Box extends Meta_Box {
 					'fields' => $fields,
 				)
 			);
+		}
+	}
+
+	public function update_term_meta( $term_id, $tt_id ) {
+		if ( is_array( $this->meta_box['fields'] ) ) {
+
+			foreach ( $this->meta_box['fields'] as $key => $meta_box_field ) {
+				if ( isset( $_POST[ $key ] ) ) {
+						update_term_meta( $term_id, $key, $_POST[ $key ] );
+				} else {
+					delete_term_meta( $term_id, $key );
+				}
+			}
 		}
 	}
 }
