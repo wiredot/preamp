9309e7948a66d2a053e06799a1d1a9cfa9aa4d0b
Paweł Fober <pfober@wiredot.com>
2019-02-01 10:17:09 +0100

    table button in editor

--- classes/Admin/Admin.php
+++ classes/Admin/Admin.php
@@ -9,7 +9,6 @@ use Wiredot\Preamp\Js\Js;
  * Meadow extension for Twig with WordPress specific functionality.
  */
 class Admin {
-
 	private $config = array();
 
 	public function __construct( $config ) {
@@ -28,16 +27,21 @@ class Admin {
 		$preamp_css = new Css( 'preamp', PREAMP_URL . 'vendor/wiredot/preamp/assets/css/preamp.css', 'admin' );
 		$preamp_css->register_css_files();
 
-		$trumbowyg_css = new Css( 'trumbowyg', PREAMP_URL . 'vendor/wiredot/preamp/assets/lib/trumbowyg/dist/ui/trumbowyg.css', 'admin' );
+		$trumbowyg_css = new Css( 'trumbowyg', PREAMP_URL . 'vendor/wiredot/preamp/src/bower/trumbowyg/dist/ui/trumbowyg.css', 'admin' );
 		$trumbowyg_css->register_css_files();
+		$trumbowyg_table_css = new Css( 'trumbowyg_table', PREAMP_URL . 'vendor/wiredot/preamp/src/bower/trumbowyg/dist/plugins/table/ui/trumbowyg.table.css', 'admin' );
+		$trumbowyg_table_css->register_css_files();
 	}
 
 	public function init_admin_js() {
 		$preamp_js = new Js( 'admin', 'preamp', PREAMP_URL . 'vendor/wiredot/preamp/assets/js/preamp.js', 'admin' );
 		$preamp_js->register_js_files();
 
-		$trumbowyg_js = new Js( 'admin', 'trumbowyg', PREAMP_URL . 'vendor/wiredot/preamp/assets/lib/trumbowyg/dist/trumbowyg.js', 'admin' );
+		$trumbowyg_js = new Js( 'admin', 'trumbowyg', PREAMP_URL . 'vendor/wiredot/preamp/src/bower/trumbowyg/dist/trumbowyg.js', 'admin' );
 		$trumbowyg_js->register_js_files();
+
+		$trumbowyg_table_js = new Js( 'admin', 'trumbowyg-table', PREAMP_URL . 'vendor/wiredot/preamp/src/bower/trumbowyg/dist/plugins/table/trumbowyg.table.js', 'admin' );
+		$trumbowyg_table_js->register_js_files();
 	}
 
 }
